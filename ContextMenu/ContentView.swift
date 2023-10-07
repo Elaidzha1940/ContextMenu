5bb1657da1a6d8fabdf13be61779b022308fc467
Elaidzha1940 <eli080293@gmail.com>
2023-10-07 18:39:00 +0300

    ContextMenu

---
+++ ContextMenu/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: ContextMenu
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 07.10.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+          
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ContextMenu/ContextMenuApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: ContextMenuApp
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 07.10.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct ContextMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
